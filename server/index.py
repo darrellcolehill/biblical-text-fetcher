f2702603ca6a0adaa87fb293541e0c1178de30cf
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-10-02 12:05:10 -0400

    Gets data from BG or GPT

--- server/index.py
+++ server/index.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, jsonify
+from bible_gateway_yoinker import bible_gateway_yoink
+from gpt_yoinker import chatgpt_yoink
 
 app = Flask(__name__)
 
 @app.route('/yoinkGPT', methods=['POST'])
 def yoink_gpt():
-    data = request.json  # Get the JSON data from the request
+    data = request.json
     if not data:
         return jsonify({"error": "No data provided"}), 400
 
     # Process the data as needed
+    gptResponse = chatgpt_yoink(data['version'], data['book'], data['chapter'], data['verses'])
+
     response = {
         "message": "Data received at yoinkGPT!",
-        "received": data
+        "text": gptResponse
     }
     return jsonify(response), 200
+
 
 @app.route('/yoinkBG', methods=['POST'])
 def yoink_bg():
@@ -21,10 +26,12 @@ def yoink_bg():
     if not data:
         return jsonify({"error": "No data provided"}), 400
 
+    bgResponse = bible_gateway_yoink(data['version'], data['book'], data['chapter'], data['verses'])
+
     # Process the data as needed
     response = {
         "message": "Data received at yoinkBG!",
-        "received": data
+        "text": bgResponse
     }
     return jsonify(response), 200
 
