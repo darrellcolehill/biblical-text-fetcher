615fa552766838f735ab91fa1cfef2d651da3819
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-10-11 09:51:23 -0400

    Added dependencies

--- cli/setup.py
+++ cli/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='bible-fetcher',
-    version='0.0.1',
+    version='0.0.2',
     description='Fetches bible passages from ChatGPT or BibleGateway',
     author='Darrell Hill',
     author_email='darrellcolehill@gmail.com',
@@ -12,6 +12,10 @@ setup(
         'console_scripts': ['bible-fetcher=fetcher.index:entry_point'],
     },
     install_requires=[
-        'requests',
+        "requests",                # For making HTTP requests
+        "beautifulsoup4",          # For parsing HTML (BeautifulSoup)
+        "python-dotenv",           # For loading environment variables from a .env file
+        "openai",                  # OpenAI library for accessing GPT APIs
+        "argparse",                # Standard Python library (already included in Python >= 3.2)
     ],
 )
